399f67b90acfcd9db865da8c2ea8e202e8afd91e
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2021-01-08 22:28:15 -0500

    added outer constructor; direct imports for type
    Added SuperFrame type with half-built support for Index type and respective AbstractDataFrame type.

--- src/SuperFrames.jl
+++ src/SuperFrames.jl
@@ -1,9 +1,22 @@
 module SuperFrames
-import DataFrames
-import DataFrames: AbstractDataFrame, Index
+# ----
+import DataFrames: AbstractDataFrame
+import DataFrames: Index
 mutable struct SuperFrame <: AbstractDataFrame
         columns::Vector{AbstractVector}
         colindex::Index
+        function SuperFrame(dict::Dict)
+                index = 0
+                names = []
+                lookup = Dict()
+                for (name, data) in dict
+                        append!(names, name)
+                        index += 1
+                        push!(lookup, name => index)
+                end
+                names = Vector{Symbol}(names)
+                index = Index(lookup, names)
+        end
 end
 # -----
 end
