85be50a485b351e402dd9ecf0aa3e7850f4db0b8
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2021-05-05 18:50:04 -0400

    changed scope, passed funcs through to type

--- src/SuperFrames.jl
+++ src/SuperFrames.jl
@@ -1,37 +1,49 @@
 module SuperFrames
 import Base: getindex, setindex!
 # ----
-mutable struct SuperFrame
+mutable struct SuperFrame{h, d}
         lookup::Dict
+        names::Array{Symbol}
+        cols::Array{Array}
+        head::h
+        drop::d
         function SuperFrame(p::Pair ...)
-                new(Dict([pair for pair in p]))
+                lookup = Dict([pair for pair in p])
+                names = Array([Symbol(key) for key in keys(lookup)])
+                cols = values(lookup)
+                cols = Array{Array}([Array(val) for val in values(lookup)])
+                head(count::Int64) = _head(lookup, count)
+                drop(slice::Int64) = _drop(lookup, slice)
+                new{typeof(head),
+                 typeof(drop)}(lookup, names, cols, head)
+        end
+        function _head(lookup::Dict, count::Int64)
+                thead = "<thead><tr>"
+                tbody = "<tbody>"
+                [thead = string(thead, "<th>", string(name),
+                 "</th>") for name in keys(lookup)]
+                 thead = string(thead, "</tr></thead>")
+                 cols = values(lookup)
+                 features = [push!(val) for val in cols]
+                 for i in 1:count
+                         obs = [row[i] for row in features]
+                         tbody = string(tbody, "<tr>")
+                         for observ in obs
+
+                         end
+                         [tbody = string(tbody, "<td>", observ,
+        "</td>") for observ in obs]
+                        tbody = string(tbody, "</tr>")
+                 end
+                 tbody = string(tbody, "</tbody>")
+                 final = string("<table>", thead, tbody, "</table")
+                 display("text/html", final)
         end
 end
 
-function head(sf::SuperFrame)
-        lookup = sf.lookup
-        thead = "<thead><tr>"
-        tbody = "<tbody>"
-        [thead = string(thead, "<th>", string(name),
-         "</th>") for name in keys(lookup)]
-         thead = string(thead, "</tr></thead>")
-         cols = values(lookup)
-         features = [push!(val) for val in cols]
-         for i in 1:length(features[1])
-                 obs = [row[i] for row in features]
-                 tbody = string(tbody, "<tr>")
-                 for observ in obs
-
-                 end
-                 [tbody = string(tbody, "<td>", observ,
-"</td>") for observ in obs]
-                tbody = string(tbody, "</tr>")
-         end
-         tbody = string(tbody, "</tbody>")
-         final = string("<table>", thead, tbody, "</table")
-         display("text/html", final)
+function _drop(lookup::Dict, slice::Int64)
+        [deleteat!(x, slice) for x in values(lookup)]
 end
-
 getindex(sf::SuperFrame, s::Symbol) = sf.lookup[s]
 setindex!(sf::SuperFrame, x::Array, s::Symbol) = push!(sf.lookup, s => x)
 
